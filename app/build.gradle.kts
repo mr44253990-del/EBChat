afaea713372262cee4ea001b6605b5d80b44cbab
mr44253990-del <mr44253990@gmail.com>
2026-07-13 15:47:00 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.5"
-    }
+    kotlinCompilerExtensionVersion = "1.5.15"
+}
 
     packaging {
         resources {
